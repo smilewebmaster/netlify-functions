e792d9f31aef971349febbbfbf42681d9bb5cdd9
bbg0x <batuhangoksu@gmail.com>
2021-02-04 13:29:05 +0300

    netlify trials

--- services/submit/main.go
+++ services/submit/main.go
@@ -5,14 +5,36 @@
 package main
 
 import (
+	"encoding/json"
+	"net/http"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
+type response struct {
+	ID     string `json:"id"`
+	UserID string `json:"userId"`
+	Login  string `json:"login"`
+	Status string `json:"status"`
+}
+
 func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	body := response{
+		ID: "ID",
+		UserID: "userID",
+		Login: "Login",
+		Status: "Status",
+	}
+	response, err := json.Marshal(&body)
+	if err != nil {
+		return events.APIGatewayProxyResponse{
+			Body: err.Error(), 
+			StatusCode: 404,
+		}, nil
+	}
 	return events.APIGatewayProxyResponse{
-		StatusCode: 200,
-		Body: "Hello AWS Lambda and Netlify",
+		StatusCode: http.StatusOK,
+		Body: string(response),
 	}, nil
 }
 
