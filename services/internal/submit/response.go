10aeb3ff8d61601b36dd43a7b74a77884ef2d184
bbg0x <batuhangoksu@gmail.com>
2021-02-04 17:06:07 +0300

    update

--- services/internal/submit/response.go
+++ services/internal/submit/response.go
@@ -5,16 +5,25 @@
 package submit
 
 import (
+	"net/http"
 	"encoding/json"
 	"github.com/aws/aws-lambda-go/events"
 )
 
 func apiResponse(status int, body interface{}) (*events.APIGatewayProxyResponse, error) {
-	resp := events.APIGatewayProxyResponse{Headers: map[string]string{"Content-Type": "application/json"}}
+	resp := events.APIGatewayProxyResponse{}
+	resp.Headers = map[string]string{"Content-Type": "application/json"}
 	resp.StatusCode = status
 	stringBody, _ := json.Marshal(body)
 	resp.Body = string(stringBody)
 	return &resp, nil
+}
+
+func clientError(status int) (events.APIGatewayProxyResponse, error) {
+    return events.APIGatewayProxyResponse{
+        StatusCode: status,
+        Body: http.StatusText(status),
+    }, nil
 }
 
 //
