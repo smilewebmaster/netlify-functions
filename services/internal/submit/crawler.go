d617f47d6bfc533e7fce9c3aa7b9cb312ac01a61
bbg0x <batuhangoksu@gmail.com>
2021-02-06 20:25:56 +0300

    update

--- services/internal/submit/crawler.go
+++ services/internal/submit/crawler.go
@@ -12,41 +12,40 @@ import (
 )
 
 type PageData struct {
-	Title     string `pagser:"title"`
-	Canonical string `pagser:"link[rel=canonical]->attr(href)"`
+	Title     string `json:"title" pagser:"title"`
+	Canonical string `json:"canonical" pagser:"link[rel=canonical]->attr(href)"`
 	Meta      struct {
-		Description string   `pagser:"meta[name='description']->attr(content)"`
-		Keywords    []string `pagser:"meta[name='keywords']->attrSplit(content)"`
+		Description string   `json:"description" pagser:"meta[name='description']->attr(content)"`
+		Keywords    []string `json:"keywords" pagser:"meta[name='keywords']->attrSplit(content)"`
 	} `pagser:"head"`
 	OpenGraph struct {
-		Locale      string `pagser:"meta[property='og:locale']->attr(content)"`
-		Type        string `pagser:"meta[property='og:type']->attr(content)"`
-		Title       string `pagser:"meta[property='og:title']->attr(content)"`
-		Description string `pagser:"meta[property='og:description']->attr(content)"`
-		Url         string `pagser:"meta[property='og:url']->attr(content)"`
-		SiteName    string `pagser:"meta[property='og:site_name']->attr(content)"`
-		Image       string `pagser:"meta[property='og:image']->attr(content)"`
+		Locale      string `json:"locale" pagser:"meta[property='og:locale']->attr(content)"`
+		Type        string `json:"type" pagser:"meta[property='og:type']->attr(content)"`
+		Title       string `json:"title" pagser:"meta[property='og:title']->attr(content)"`
+		Description string `json:"description" pagser:"meta[property='og:description']->attr(content)"`
+		Url         string `json:"url" pagser:"meta[property='og:url']->attr(content)"`
+		SiteName    string `json:"site_name" pagser:"meta[property='og:site_name']->attr(content)"`
+		Image       string `json:"image" pagser:"meta[property='og:image']->attr(content)"`
 	} `pagser:"head"`
 }
 
-func fatal(err error) {
+func isFatal(err error) {
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
-func crawler() PageData {
-	response, err := http.Get("https://medium.com/swlh/write-a-custom-reusable-hook-usefetch-1443d8d4e1e1")
-	fatal(err)
+var data PageData
+
+func crawler(siteUrl string) PageData {
+	response, err := http.Get(siteUrl)
+	isFatal(err)
 	defer response.Body.Close()
-	page := pagser.New()
-	var data PageData
-	// parse html data
-	err = page.ParseReader(&data, response.Body)
+	html := pagser.New()
+	err = html.ParseReader(&data, response.Body) // parse html data
 	// check error
-	fatal(err)
-	// print data
-	return data
+	isFatal(err)
+	return data // print data
 }
 
 //
--- services/internal/submit/main_test.go
+++ services/internal/submit/main_test.go
@@ -36,3 +36,19 @@ func TestApiHandler(t *testing.T) {
 		})
 	}
 }
+
+func TestApiResponse(t *testing.T) {
+	tests := []struct {
+		name string
+		want Response
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := ApiResponse(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("ApiResponse() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
